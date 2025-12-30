5ae0cd97f7dc9b8320cb3dbb899e06b7262ed5cd
Akinwalire Olanrewaju <olanrewajuakinwalire@gmail.com>
2025-12-30 12:11:24 +0100

    Fixed Blogs Page

---
+++ src/app/blogs/page.js
@@ -0,0 +1,18 @@
+import NavAndFooterWrap from "@/components/wrapper/Index";
+
+export default function BlogsPage() {
+  return (
+    <>
+      <NavAndFooterWrap>
+        <div className="container mx-auto">
+          <main
+            className="blog-container flex justify-center items-center h-[100vh]
+          "
+          >
+            <p className="text-[50px]">NOTHING TO SEE HERE YET</p>
+          </main>
+        </div>
+      </NavAndFooterWrap>
+    </>
+  );
+}
